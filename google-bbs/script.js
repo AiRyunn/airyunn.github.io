f5aec0ad180bd638a579440639f27c793bf9999e
AiRyunn <espylapiza@gmail.com>
2023-09-20 10:23:23 -0700

    Update UI

--- google-bbs/script.js
+++ google-bbs/script.js
@@ -1,4 +1,4 @@
-(function () {
+function init_cse() {
     var cx = 'f2db1948f357f45d9';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
@@ -6,4 +6,42 @@
     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
-})();
+};
+
+function init_search_box() {
+    const searchButton = document.getElementById('search-button');
+    const searchInput = document.getElementById('search-input');
+    const urlParams = new URLSearchParams(window.location.search);
+    const query = urlParams.get('q');
+
+    if (query !== null) {
+        searchInput.value = decodeURIComponent(query);
+    }
+
+    function executeSearch() {
+        const updatedQuery = searchInput.value;
+        window.location.href = window.location.pathname + '?q=' + encodeURIComponent(updatedQuery);
+    }
+
+    searchButton.addEventListener('click', executeSearch);
+
+    searchInput.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            executeSearch();
+        }
+    });
+}
+
+
+function init() {
+    init_cse();
+    init_search_box();
+}
+
+if (document.readyState !== 'loading') {
+    init();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        init();
+    });
+}
